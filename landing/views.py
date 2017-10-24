0b635e0c4d4c026cd5ccef19f5c5ee8d4fef1dac
rocket <miyavi931@gmail.com>
2017-10-24 14:49:47 +0600

    djtest

--- landing/views.py
+++ landing/views.py
@@ -12,4 +12,5 @@ def home(request):
     products_images = ProductImage.objects.filter(is_active=True, is_main=True, product__is_active=True)
     products_images_phones = products_images.filter(product__category__id=2)
     products_images_laptop = products_images.filter(product__category__id=3)
+    products_images_personal = products_images.filter(product__category__id=4)
     return render(request, 'home.html', locals())
--- orders/models.py
+++ orders/models.py
@@ -45,6 +45,7 @@ class ProductInOrder (models.Model):
     order = models.ForeignKey(Order, blank=True, null=True, default=None)
     product = models.ForeignKey(Product, blank=True, null=True, default=None)
     nmb = models.IntegerField(default=1)
+    is_active = models.BooleanField(default=True)
     price_per_item = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)#price*nmb
     created = models.DateTimeField(auto_now_add=True, auto_now=False)
@@ -67,14 +68,14 @@ class ProductInOrder (models.Model):
         super(ProductInOrder, self).save(*args, **kwargs)
 
 # @disable_for_loaddata
-# def product_in_order_post_save(sender, instance, created, **kwargs):
-#     order = instance.order
-#     all_products_in_order = ProductInOrder.objects.filter(order=order, is_active=True)
-#     order_total_price = 0
-#     for item in all_products_in_order:
-#         order_total_price +=item.total_price
-#
-#     instace.order.total.price = order_total_price
-#     instance.order.save(force_update=True)
-#
-# post_save.connect(product_in_order_post_save, sender = ProductInOrder)
+def product_in_order_post_save(sender, instance, created, **kwargs):
+    order = instance.order
+    all_products_in_order = ProductInOrder.objects.filter(order=order, is_active=True)
+    order_total_price = 0
+    for item in all_products_in_order:
+        order_total_price +=item.total_price
+
+    instance.order.total_price = order_total_price
+    instance.order.save(force_update=True)
+
+post_save.connect(product_in_order_post_save, sender = ProductInOrder)
